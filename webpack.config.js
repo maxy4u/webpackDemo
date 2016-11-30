2260e575dad3d11e3de9fd18c07d3c340a3cce0b
gkhurana@ca.ibm.com <gkhurana@ca.ibm.com>
2016-11-30 15:53:08 -0400

    organising files and folders in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
+var path = require('path');
 module.exports = {
+	context:path.resolve('js'),
 	entry:["./utils","./app.js"],
 	output:{
+		path:path.resolve('build/js/'),
+		publicPath:'/public/assets/js/',
 		filename:"bundle.js"
+	},
+	devServer:{
+		contentBase:'public'
 	},
 	//watch:true,
 	module:{
-		preLoaders:[{
+		/*preLoaders:[{
 			test:/.js$/,
 			exclude:/node_modules/,
 			loader:"jshint-loader"
-		}],
+		}],*/
 		loaders:[{
 			test:/\.es6$/,
 			exclude:/node_modules/,
